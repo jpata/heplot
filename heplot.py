a49dabd8ca4685b68978c65fc4a2d6884a6a3563
Joosep <joosep.pata@gmail.com>
2015-02-04 11:41:01 +0100

    added more tests

--- heplot.py
+++ heplot.py
@@ -1,10 +1,11 @@
+import matplotlib
+matplotlib.use('Agg')
 import rootpy.plotting.root2matplotlib as rplt
 import matplotlib.pyplot as plt
-from rootpy.plotting import Hist, Hist2D
+from rootpy.plotting import Hist, Hist2D, HistStack
 from rootpy.io.file import root_open
 import sys
 import numpy as np
-import matplotlib
 import matplotlib.gridspec as gridspec
 
 from matplotlib.ticker import AutoMinorLocator
@@ -14,8 +15,37 @@ matplotlib.rc('font', serif='Arial')
 matplotlib.rc('text', usetex='true') 
 matplotlib.rcParams.update({'font.size': 16})
 
+def normalize(h):
+    if (h.Integral() > 0):
+        h.Scale(1.0 / h.Integral())
+
+def as_array(x):
+    
+#     dims = []
+#     if x.GetNbinsX()>0:
+#         dims += [x.GetNbinsX()+2]
+#     if x.GetNbinsY()>0:
+#         dims += [x.GetNbinsY()+2]
+#     if x.GetNbinsZ()>0:
+#         dims += [x.GetNbinsZ()+2]
+    arr = np.zeros((x.GetNbinsX(), x.GetNbinsY(), x.GetNbinsZ()), dtype=np.float64)
+    for i in range(1, x.GetNbinsX() + 1):
+        for j in range(1, x.GetNbinsY() + 1):
+            for k in range(1, x.GetNbinsZ() + 1):
+                arr[i-1, j-1, k-1] = x.GetBinContent(i,j,k)
+    # print arr
+    if x.GetNbinsZ() == 1:
+        arr = arr.sum(2)
+    elif x.GetNbinsY() == 1 and x.GetNbinsZ() == 1:
+        arr = arr.sum((1,2))
+    elif x.GetNbinsZ() == 1:
+        arr = arr.sum(2)
+    elif x.GetNbinsY() == 1:
+        arr = arr.sum(1)
+    return arr#.sum(1).sum(1)
+
 def barhist(h, **kwargs):
-    col = kwargs.pop("color")
+    col = kwargs.pop("color", "blue")
 
     #b = rplt.errorbar(h, xerr=False, color=col, mec=col, ms=0, ecolor=col, **kwargs)
     lw = kwargs.pop("lw", 1)
@@ -35,15 +65,38 @@ def barhist(h, **kwargs):
         plt.gca().add_patch(_b)
     #kwargs_d.pop("lw")
     kwargs_d.pop("edgecolor", "")
+    kwargs_d.pop("stacked", "")
     #plt.xticks(np.arange(min(xs), max(xs), (max(xs)-min(xs)) / ))
     plt.plot(xs, ys, color=col, lw=lw, label=lab, **kwargs_d)
     return b
 
-def matshow(ax, hpt, **kwargs):
-    return ax.matshow(hpt, cmap="Greys", interpolation="none", origin="lower",
-    extent=[hpt.GetXaxis().GetBinLowEdge(1), hpt.GetXaxis().GetBinLowEdge(hpt.GetNbinsX()),
-            hpt.GetYaxis().GetBinLowEdge(1), hpt.GetYaxis().GetBinLowEdge(hpt.GetNbinsY())])
+def stackhist(histograms, **kwargs):
+    stack = HistStack(histograms, drawstyle='HIST E1 X0')
+    b = barhist(stack, stacked=True)
+    return b
 
+def matshow(ax, hpt, **kwargs):
+    arr = as_array(hpt)
+    #print arr
+
+    #Need to rotate 90 degrees ccw to conform to ROOT-s standard
+    arr = np.rot90(arr) 
+    #print arr
+    assert(len(arr.shape) == 2) 
+    ret = ax.matshow(arr,
+        cmap="hot",
+        interpolation="none",
+        #origin="lower",
+        aspect="auto",
+        extent=[
+            hpt.GetXaxis().GetBinLowEdge(1),
+            hpt.GetXaxis().GetBinLowEdge(hpt.GetNbinsX()) + hpt.GetXaxis().GetBinWidth(hpt.GetNbinsX()),
+            hpt.GetYaxis().GetBinLowEdge(1),
+            hpt.GetYaxis().GetBinLowEdge(hpt.GetNbinsY()) + hpt.GetYaxis().GetBinWidth(hpt.GetNbinsY()),
+        ]
+    )
+    
+    return ret
 
 def ratio_axes(fig):
     gs = gridspec.GridSpec(2, 1, height_ratios=[2,1])
--- test.py
+++ test.py
@@ -6,14 +6,20 @@ import matplotlib.pyplot as plt
 import random
 import matplotlib
 
+#radiator palette
+ROOT.gROOT.ProcessLine("gStyle->SetPalette(53, 0);")
+
 #to convert ROOT objects to rootpy objects
 from rootpy import asrootpy
 
+tf = ROOT.TFile("test.root", "RECREATE")
+tf.cd()
+
 ####################################
-h1 = ROOT.TH1D("a", "a", 20, -2, 2) 
+h1 = ROOT.TH1D("a1", "a", 20, -2, 2) 
 h1 = asrootpy(h1)
 
-h2 = ROOT.TH1D("a", "a", 20, -2, 2) 
+h2 = ROOT.TH1D("a2", "a", 20, -2, 2) 
 h2 = asrootpy(h2)
 
 for i in range(10000):
@@ -30,8 +36,11 @@ plt.savefig("h1.png")
 plt.close()
 ####################################
 
-fig = plt.figure(figsize=(8,8))
 
+####################################
+# Ratio, 2 histograms
+####################################
+fig = plt.figure(figsize=(8,8))
 ax1, ax2 = he.ratio_axes(fig)
 
 plt.sca(ax1)
@@ -48,4 +57,44 @@ plt.axhline(1.0, color="blue", lw=2)
 plt.xlabel("$ p_t $ of the leading jet [GeV]")
 plt.ylabel("ratio")
 plt.savefig("h2.png")
+plt.close()
+
+tc = ROOT.TCanvas()
+h1.Draw()
+h2.Draw("SAME")
+tc.SaveAs("h2_r.png")
+
+####################################
+# 2D histogram
+####################################
+fig = plt.figure(figsize=(10,8))
+ax = plt.axes()
+
+h3 = ROOT.TH2D("h3", "h", 500, -2, 2, 250, -3, 3)
+for i in range(1000000):
+        h3.Fill(random.gauss(0.5, 0.8), random.gauss(0.0, 1.0))
+mat = he.matshow(ax, h3)
+fig.colorbar(mat)
+fig.tight_layout()
+plt.savefig("h3.png")
+plt.close()
+
+tc = ROOT.TCanvas("a", "a", 800, 800)
+h3.SetStats(False)
+h3.Draw("COLZ")
+tc.SaveAs("h3_r.png")
+
+tf.Write()
+tf.Close()
+
+
+####################################
+# Stack
+####################################
+fig = plt.figure(figsize=(8,8))
+ax1, ax2 = he.ratio_axes(fig)
+
+plt.sca(ax1)
+b1 = he.stackhist([h1, h2])
+plt.savefig("stack.png")
 plt.close()
